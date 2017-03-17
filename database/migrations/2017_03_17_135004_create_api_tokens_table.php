2525c2e09dd9b3c3b1c386b08265e50522032d81
Gabor Javorszky <gabor@javorszky.co.uk>
2017-03-17 13:57:26 +0000

    Added details to api_tokens migrations

--- database/migrations/2017_03_17_135004_create_api_tokens_table.php
+++ database/migrations/2017_03_17_135004_create_api_tokens_table.php
@@ -16,14 +16,17 @@ class CreateApiTokensTable extends Migration
             $table->string('id')->primary();
             $table->integer('user_id');
             $table->string('name');
+            $table->string('site', 191)->nullable();
             $table->string('token', 100)->unique();
+            // $table->string('secret', 100)->unique();
+            $table->string('secret', 100)->nullable();
             $table->text('metadata');
             $table->tinyInteger('transient')->default(0);
             $table->timestamp('last_used_at')->nullable();
             $table->timestamp('expires_at')->nullable();
             $table->timestamps();
 
-            $table->index(['user_id', 'expires_at']);
+            $table->index(['user_id', 'expires_at', 'site']);
         });
     }
 
