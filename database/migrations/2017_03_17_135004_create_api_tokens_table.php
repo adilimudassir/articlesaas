07680ed450abb60a41037931f1ca0d5ceb997187
Gabor Javorszky <gabor@javorszky.co.uk>
2017-03-17 18:34:57 +0000

    Lock down api_tokens database table

--- database/migrations/2017_03_17_135004_create_api_tokens_table.php
+++ database/migrations/2017_03_17_135004_create_api_tokens_table.php
@@ -16,10 +16,9 @@ class CreateApiTokensTable extends Migration
             $table->string('id')->primary();
             $table->integer('user_id');
             $table->string('name');
-            $table->string('site', 191)->nullable();
+            $table->string('site', 191);
             $table->string('token', 100)->unique();
-            // $table->string('secret', 100)->unique();
-            $table->string('secret', 100)->nullable();
+            $table->string('secret', 100)->unique();
             $table->text('metadata');
             $table->tinyInteger('transient')->default(0);
             $table->timestamp('last_used_at')->nullable();
